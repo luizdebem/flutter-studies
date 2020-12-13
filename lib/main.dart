5b8a3a07b10936b14534f0a9aeeeb89191ea0c4b
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-12-13 19:27:14 -0300

    Home Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: Text(
-            'Welcome to my App!',
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2,
-                color: Colors.grey[600],
-                fontFamily: 'IndieFlower'),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('bom dia');
-          },
-          child: Text('?'),
-          backgroundColor: Colors.red[600],
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to my App!',
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('bom dia');
+        },
+        child: Text('?!'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
