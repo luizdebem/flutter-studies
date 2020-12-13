d6afb81bfdd673912afad2e95140632e314b2baa
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-12-13 19:17:41 -0300

    first commit: styling and added font

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+          child: Text(
+            'Welcome to my App!',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+                color: Colors.grey[600],
+                fontFamily: 'IndieFlower'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('bom dia');
+          },
+          child: Text('?'),
+          backgroundColor: Colors.red[600],
+        ),
+      ),
+    ));
