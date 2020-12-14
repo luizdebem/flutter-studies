57b43fbb617188551e69fe40193bce8a7d0fb664
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-12-14 14:56:08 -0300

    Ninja ID Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,72 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(MaterialApp(home: NinjaCard()));
 
-class Home extends StatelessWidget {
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My App'),
+        title: Text('Ninja ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
       ),
-      body: Row(
-        children: [
-          // Expanded(child: Image.asset('assets/bob-esponja.jpg'), flex: 1),
-          Expanded(
-            flex: 2,
-            child: Container(
-                padding: EdgeInsets.all(30),
-                color: Colors.cyan,
-                child: Text('2/4')),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-                padding: EdgeInsets.all(30),
-                color: Colors.pinkAccent,
-                child: Text('1/4')),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-                padding: EdgeInsets.all(30),
-                color: Colors.amber,
-                child: Text('1/4')),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('bom dia');
-        },
-        child: Text('?!'),
-        backgroundColor: Colors.red[600],
+      backgroundColor: Colors.grey[900],
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://raw.githubusercontent.com/iamshaunjp/flutter-beginners-tutorial/lesson-15/ninja_id/assets/thumb.jpg'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 90,
+              color: Colors.grey[800],
+            ),
+            Text('NAME',
+                style: TextStyle(color: Colors.grey, letterSpacing: 2)),
+            SizedBox(height: 10),
+            Text('Chun Li',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: 30),
+            Text('CURRENT NINJA LEVEL',
+                style: TextStyle(color: Colors.grey, letterSpacing: 2)),
+            SizedBox(height: 10),
+            Text('25',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('chunli@dev.com',
+                    style: TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 18,
+                        letterSpacing: 1))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
