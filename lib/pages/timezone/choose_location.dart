25b335e541c5f4d5c0115d845357294c3f68b86c
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-12-22 01:28:37 -0300

    Weather App MVP

--- lib/pages/timezone/choose_location.dart
+++ lib/pages/timezone/choose_location.dart
@@ -9,24 +9,61 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getCities() async {}
+  Future<Map> getData(String city) async {
+    String API_KEY = DotEnv().env['WEATHER_API_KEY'];
+    String URL_API =
+        'http://api.openweathermap.org/data/2.5/weather?q=$city&appid=$API_KEY&units=metric';
+    Response res = await get(URL_API);
+    Map data = jsonDecode(res.body);
+    return data;
+  }
 
   @override
   void initState() {
     super.initState();
-    getCities();
   }
 
   @override
   Widget build(BuildContext context) {
     print('build() triggered');
     return Scaffold(
-        backgroundColor: Colors.grey[900],
-        appBar: AppBar(
-          title: Text('Choose location'),
-          centerTitle: true,
-          backgroundColor: Colors.grey[850],
-          elevation: 0,
-        ));
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Choose location'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: TextFormField(
+            style: TextStyle(color: Colors.white),
+            maxLength: 35,
+            expands: false,
+            decoration: const InputDecoration(
+                hintStyle: TextStyle(color: Colors.amberAccent),
+                counterStyle: TextStyle(color: Colors.amberAccent),
+                hintText: 'Enter a city!',
+                fillColor: Colors.white),
+            onFieldSubmitted: (String city) async {
+              try {
+                Map data = await getData(city);
+                if (data['cod'] == 200) {
+                  Navigator.pushReplacementNamed(context, '/timezone', arguments: {
+                    'cityName': data['name'],
+                    'temperature': data['main']['temp'].toString()
+                  });
+                } else {
+                  // TODO tratar erros;
+                }
+              } catch (err) {
+                // TODO tratar erros;
+              }
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/pages/timezone/timezone.dart
+++ lib/pages/timezone/timezone.dart
@@ -9,6 +9,13 @@ class Timezone extends StatefulWidget {
 class _TimezoneState extends State<Timezone> {
   @override
   Widget build(BuildContext context) {
+    String cityName = '';
+    String temperature = '';
+    if (ModalRoute.of(context).settings.arguments != null) {
+      Map data = ModalRoute.of(context).settings.arguments;
+      cityName = data['cityName'];
+      temperature = data['temperature'] + '° C';
+    }
     return Scaffold(
         backgroundColor: Colors.grey[900],
         appBar: AppBar(
@@ -18,16 +25,34 @@ class _TimezoneState extends State<Timezone> {
           elevation: 0,
         ),
         drawer: Sidebar(),
-        body: Column(
-          children: [
-            FlatButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location, color: Colors.amberAccent[200],),
-              label: Text('Edit Location', style: TextStyle(color: Colors.amberAccent[200])),
-            )
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FlatButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(
+                  Icons.edit_location,
+                  color: Colors.amberAccent[200],
+                ),
+                label: Text('Edit Location',
+                    style: TextStyle(color: Colors.amberAccent[200])),
+              ),
+              Text(
+                cityName,
+                style: TextStyle(color: Colors.white, fontSize: 26),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                temperature,
+                style: TextStyle(color: Colors.white, fontSize: 58),
+              )
+            ],
+          ),
         ));
   }
 }
