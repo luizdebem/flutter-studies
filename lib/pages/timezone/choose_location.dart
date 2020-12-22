bcc3f51331fec83ebca921edb2fdfc93b94408cb
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-12-21 23:26:04 -0300

    async await

--- lib/pages/timezone/choose_location.dart
+++ lib/pages/timezone/choose_location.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:myapp_flutter/components/Sidebar/sidebar.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -7,33 +6,32 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
-  
+
+  void getData() async {
+    // simulando uma requisição..
+    await Future.delayed(Duration(seconds: 3), () { // tipo o setTimeout, vai rodar a função após os segundos.
+      print('Response from HTTP Request');
+    });
+    print('Linha após a "requisição"');
+  }
+
   @override
   void initState() {
     super.initState();
     print('initState() triggered');
+    getData();
   }
 
   @override
   Widget build(BuildContext context) {
     print('build() triggered');
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('Choose location'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0,
-      ),
-      body: RaisedButton(
-        onPressed: () {
-          setState(() {
-            counter++;
-          });
-        },
-        child: Text('Counter: $counter'),
-      ),
-    );
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text('Choose location'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0,
+        ));
   }
 }
