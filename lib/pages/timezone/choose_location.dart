8d9f4870c8d3587962b7f1ed012dcf6edc147068
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-12-21 23:36:12 -0300

    HTTP request & JSON decoding

--- lib/pages/timezone/choose_location.dart
+++ lib/pages/timezone/choose_location.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert'; // jsonDecode();
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -8,11 +10,9 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
 
   void getData() async {
-    // simulando uma requisição..
-    await Future.delayed(Duration(seconds: 3), () { // tipo o setTimeout, vai rodar a função após os segundos.
-      print('Response from HTTP Request');
-    });
-    print('Linha após a "requisição"');
+    Response res = await get('https://jsonplaceholder.typicode.com/todos/1');
+    Map data = jsonDecode(res.body);
+    print(data['userId']);
   }
 
   @override
